24fccb93b45d8575e822588a21111400b5ba9adb
ZareMendozaC <zare.mendozac@gmail.com>
2016-12-21 15:05:03 -0500

    adding form, styles, responsive design and javascript funtions

--- main.js
+++ main.js
@@ -1,74 +1,25 @@
-function costoPorLlamadas() {
-	var num1 = parseInt(document.getElementById('n1').value);
-	var num2 = parseFloat(document.getElementById('n2').value);
-    document.getElementById('resultado').value = num1 * num2;
-}
-
-
-function alcanzoPromedio(puntosSquad, maxPuntos) {
-    var resultado=0;
-    resultado=(puntosSquad*100)/maxPuntos;
+function alcanzoPromedio() {
+	var PuntosSquad = parseInt(document.getElementById("puntosSquad").value);
+	var MaxPuntos = parseInt(document.getElementById("maxPuntos").value);
+    var frase;
+    resultado=(PuntosSquad*100)/MaxPuntos;
     if(resultado>80)
-        {
-            return("Muy bien squad!")
+        { 
+            document.getElementById("frase").innerHTML="<p class='frase'>Muy bien squad!</p>";
         }
     else
+    {
         if(resultado<80)
             {
-            return("Pueden hacerlo mejor.")
+             document.getElementById("frase").innerHTML="<p class='frase'>Pueden hacerlo mejor.</p>";
             }
-}
-function sleeping(weekday, vacations) {
-    weekday = Boolean(weekday);
-    vacations = Boolean(vacations);
-    if((weekday==false) || (vacations==true)){
-        return 1;
     }
-    else return 0;
-}
-function monkeyTrouble(aSmile, bSmile) {
-    aSmile = Boolean(aSmile);
-    bSmile = Boolean(bSmile);
-    if((aSmile==true)&(bSmile==true))
-        return 1; 
-    if((aSmile==false)&(bSmile==false))
-        return 1;
-    else 
-        return 0;
-}
-function diff21(n) {
-
-if (n<21)
-    return 21-n;
-    else
-        return 2*(n-21);
-}
-function parrotTrouble(talking, hora) {
-    talking = Boolean(talking);
-            
-            if((hora<7||hora>20)&&(talking==true))
-                return 1;
-            else{
-                return 0;
-            }
-        
+    document.getElementById('resultado').value = resultado;
 }
 
-function make10(a, b) {
-
-        if(( ((a==10 && b!=10)) || ((a!=10 && b==10)) ) || (a+b==10))
-            return 1;
-        else 
-        return 0;
-}
-
-function posNeg(a, b, negative) {
-    negative = Boolean(negative);
-    if( (a<0 && b>0) || (a>0 && b<0) )
-        return 1;
-    else 
-        if( (negative==true)&& (a<0 && b<0) )
-            return 1;
-    else 
-        return 0;
+function limpiar(){
+	 PuntosSquad = document.getElementById("puntosSquad").value="";
+	 MaxPuntos = document.getElementById("maxPuntos").value="";
+	 resultado = document.getElementById('resultado').value = "";
+	var frase = document.getElementById("frase").innerHTML="";
 }
